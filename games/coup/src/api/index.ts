42c3a54800573eae4176a047b7d8d02e52dc1540
Tom Dawes <tomdawes91@gmail.com>
2020-03-20 14:03:23 +0000

    API

--- games/coup/src/api/index.ts
+++ games/coup/src/api/index.ts
@@ -1,22 +1,80 @@
 export interface Config {
-  expansion
+  useExpansion: boolean;
+  numberOfEachCard: number;
 }
 
-export type Board = (boolean | null)[][];
+export type Move =
+  | {
+      type:
+        | "income"
+        | "foreign-aid"
+        | "tax"
+        | "exchange-1"
+        | "exchange-2"
+        | "convert"
+        | "embezzle";
+    }
+  | {
+      type: "coup" | "assassinate" | "examine" | "steal" | "convert";
+      target: string;
+    };
+
+export type Card =
+  | "duke"
+  | "assassin"
+  | "ambassador"
+  | "contessa"
+  | "captain"
+  | "inquisitor";
+
+export type Team = "loyalist" | "reformist";
+
+export type Hand = {
+  liveCards: Card[];
+  deadCards: Card[];
+  money: number;
+  team: Team;
+};
 
 export interface State {
-  board: Board;
-  firstPlayer: string;
-  firstPlayersTurn: boolean;
-  finished: boolean;
-  winner?: string;
+  players: { [key: string]: Hand };
+  deck: Card[];
+  treasury: number;
+  stack: {
+    action: PlayAction | ReactAction | ChallengeAction;
+    committed: boolean;
+    resolved: boolean;
+  }[];
 }
 
 export interface PlayAction {
   type: "play";
   playerId: string;
-  x: number;
-  y: number;
+  action: Move;
+}
+
+export interface ReactAction {
+  type: "react";
+  playerId: string;
+  card: Card;
+}
+
+export interface ChallengeAction {
+  type: "challenge";
+  playerId: string;
+}
+
+export interface RevealAction {
+  type: "reveal";
+  playerId: string;
+  target: string;
+  card: Card;
+}
+
+export interface DiscardAction {
+  type: "discard";
+  playerId: string;
+  cards: Card[];
 }
 
 export type Action = PlayAction;
