0243a2f0140387d5fa641b21ab2bb79cdf2e3c87
Andreja <andreja.kogovsek1@gmail.com>
2020-03-25 11:04:25 +0000

    Make some constants more dynamic and move into api for now

--- games/hanabi/src/api/index.ts
+++ games/hanabi/src/api/index.ts
@@ -6,18 +6,22 @@ export interface Config {
 }
 
 // TODO: rainbow
-export type Color = "red" | "blue" | "green" | "yellow" | "white";
+export const colours = ["red", "blue", "green", "yellow", "white"] as const;
+export type Colour = typeof colours[number];
+export const numbers = [1, 2, 3, 4, 5] as const;
+export type Number = typeof numbers[number];
+export const maxCardNum = numbers[numbers.length - 1];
 
 export interface Card {
-  color: Color;
+  color: Colour;
   num: number;
 }
 
 export interface Board {
   deck: Card[];
   hands: Card[][];
-  piles: { [key in Color]: number };
-  discardPile: { [key in Color]: Card[] };
+  piles: { [key in Colour]: number };
+  discardPile: { [key in Colour]: Card[] };
   fuseTokens: number;
   infoTokens: number;
 }
@@ -62,3 +66,27 @@ export type Action =
   | InfoAction
   | MoveAction
   | SelectAction;
+
+export const cardsPerNumber: { [key in Number]: number } = {
+  1: 3,
+  2: 2,
+  3: 2,
+  4: 2,
+  5: 1,
+};
+
+export const minPlayers = 2;
+export const maxPlayers = 5;
+export const noSelectedCards = new Array(maxPlayers).fill([]);
+export const getHandSize = (numPlayers: number) => (numPlayers >= 4 ? 4 : 5);
+
+const mapToColours = <T>(defaultValue: T): { [key in Colour]: T } => {
+  const piles: { [key: string]: T } = {};
+  colours.map(colour => {
+    piles[colour] = defaultValue;
+  });
+  return piles as { [key in Colour]: T };
+};
+
+export const emptyPiles: { [key in Colour]: number } = mapToColours(0);
+export const emptyDiscardPile: { [key in Colour]: Card[] } = mapToColours([]);
